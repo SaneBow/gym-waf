7615cffaca191e578da8f57fd12aac3320dae7cf
X.B. Wang <sanebow@gmail.com>
2020-12-11 16:19:33 +0800

    wrap with monitor

--- train_stable_baselines.py
+++ train_stable_baselines.py
@@ -1,4 +1,14 @@
 from stable_baselines import PPO2
-import gym_waf
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.bench import Monitor
+import gym_waf, gym
+import os, time
 
-model = PPO2('MlpPolicy', 'WafBrain-v0', verbose=1).learn(10000)
+# Create unique log dir
+log_dir = "/tmp/gym/{}".format(int(time.time()))
+os.makedirs(log_dir, exist_ok=True)
+
+env = gym.make('WafBrain-v0')
+env = Monitor(env, log_dir, allow_early_resets=True)
+env = DummyVecEnv([lambda: env])
+model = PPO2('MlpPolicy', env, verbose=1).learn(10000)
