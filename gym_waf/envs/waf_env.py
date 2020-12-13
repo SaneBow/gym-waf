1a881a63afb244ef8eb49d13a888f005208ca9e3
Han HU <huhanwj@gmail.com>
2020-12-13 13:49:48 +0800

    fixing a encoding issue on win

--- gym_waf/envs/waf_env.py
+++ gym_waf/envs/waf_env.py
@@ -40,7 +40,7 @@ class WafEnv(gym.Env):
 
     def _load_payloads(self, filepath):
         try:
-            with open(filepath, 'r') as f:
+            with open(filepath, 'rb') as f:
                 self.payload_list = f.read().splitlines()
                 logging.debug("{} payloads loaded".format(len(self.payload_list)))
         except OSError as e:
