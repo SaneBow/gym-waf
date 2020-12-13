f773f25952efca48be1cf386a261e19073eddd62
X.B. Wang <sanebow@gmail.com>
2020-12-13 10:59:55 +0800

    seperate out training scripts, this will only be a clean gym env

--- setup.py
+++ setup.py
@@ -2,9 +2,5 @@ from setuptools import setup
 
 setup(name='gym_waf',
       version='0.0.1',
-      install_requires=['gym','numpy','sklearn','requests','keras-rl2','termcolor','libinjection-python']  # And any other dependencies the package needs
-)  
-
-# note: must install https://github.com/lief-project/LIEF/releases/download/0.7.0/linux_lief-0.7.0_py3.6.tar.gz [modify for your platform] manually
-# pip install https://github.com/lief-project/LIEF/releases/download/0.7.0/linux_lief-0.7.0_py3.6.tar.gz [modify for your platform]
-
+      install_requires=['gym', 'numpy', 'termcolor', 'libinjection-python', 'waf-brain', 'sqlparse']
+      )
